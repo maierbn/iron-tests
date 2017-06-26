9bb16eb942fe66c031b0ab7a418986239aa43181
Benjamin Maier <benjamin.maier@ipvs.uni-stuttgart.de>
2017-06-26 15:53:58 +0200

    add example template for dirichlet boundary conditions

---
+++ examples/example-0402/run_example.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+echo "compiling and running example $(pwd)"
+
+folder=$1
+
+mkdir -p $folder
+
+echo "  compiling $folder"
+cd $folder
+cmake -DCMAKE_BUILD_TYPE=$folder -DOPENCMISS_BUILD_TYPE=$folder ..
+make
+cd ..
+echo "  running $folder"
+# <number elements X> <number elements Y> <interpolation type> <solver type> <PDE step size> <stop time> <output frequency> <CellML Model URL>
+#mkdir -p results/current_run/l1x1_n10x10_i1_s0 && ./$folder/src/example 10 10 1 0 0.05 1.0 10 n98.xml
+mkdir -p results/current_run/l1x1_n10x10_i1_s0 && ./$folder/src/example 10 10 1 0 0.05 10.0 10 hodgkin_huxley_1952_stim.cellml
