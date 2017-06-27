e139c4db18444e210f851a2eb9570e9507957af8
Mylena Mordhorst <mylena.mordhorst@mechbau.uni-stuttgart.de>
2017-06-27 15:51:07 +0200

    started to modify GeneralisedLaplace - running but needs to be checked properly

--- examples/example-0011/run_example.sh
+++ examples/example-0011/run_example.sh
@@ -13,28 +13,28 @@ make
 cd ..
 echo "  running $folder"
 echo "    2D tests"
-mkdir -p results/current_run/l2x1x0_n2x1x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 1 0 1 1
-mkdir -p results/current_run/l2x1x0_n4x2x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 1 0 1 1
-mkdir -p results/current_run/l2x1x0_n8x4x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 1 0 1 1
-mkdir -p results/current_run/l2x1x0_n2x1x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 2 0 1 1
-mkdir -p results/current_run/l2x1x0_n4x2x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 2 0 1 1
-mkdir -p results/current_run/l2x1x0_n8x4x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 2 0 1 1
-mkdir -p results/current_run/l2x1x0_n2x1x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 1 1 1 1
-mkdir -p results/current_run/l2x1x0_n4x2x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 1 1 1 1
-mkdir -p results/current_run/l2x1x0_n8x4x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 1 1 1 1
-mkdir -p results/current_run/l2x1x0_n2x1x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 2 1 1 1
-mkdir -p results/current_run/l2x1x0_n4x2x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 2 1 1 1
-mkdir -p results/current_run/l2x1x0_n8x4x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 2 1 1 1
-echo "    3D tests"
-mkdir -p results/current_run/l2x1x1_n2x1x1_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 1 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n4x2x2_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 1 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n8x4x4_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 1 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n2x1x1_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 2 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n4x2x2_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 2 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n8x4x4_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 2 0 1 1 1
-mkdir -p results/current_run/l2x1x1_n2x1x1_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 1 1 1 1 1
-mkdir -p results/current_run/l2x1x1_n4x2x2_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 1 1 1 1 1
-mkdir -p results/current_run/l2x1x1_n8x4x4_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 1 1 1 1 1
-mkdir -p results/current_run/l2x1x1_n2x1x1_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 2 1 1 1 1
-mkdir -p results/current_run/l2x1x1_n4x2x2_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 2 1 1 1 1
-mkdir -p results/current_run/l2x1x1_n8x4x4_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 2 1 1 1 1
+mkdir -p results/current_run/l2x1x0_n2x1x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n4x2x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n8x4x0_i1_s0 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n2x1x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n4x2x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n8x4x0_i2_s0 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n2x1x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n4x2x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n8x4x0_i1_s1 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n2x1x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 2 1 0 2 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n4x2x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 4 2 0 2 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x0_n8x4x0_i2_s1 && ./$folder/src/example 2.0 1.0 0.0 8 4 0 2 1 1 1 1 0 0 0
+#echo "    3D tests"
+#mkdir -p results/current_run/l2x1x1_n2x1x1_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n4x2x2_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n8x4x4_i1_s0 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 1 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n2x1x1_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n4x2x2_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n8x4x4_i2_s0 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 2 0 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n2x1x1_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n4x2x2_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n8x4x4_i1_s1 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 1 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n2x1x1_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 2 1 1 2 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n4x2x2_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 4 2 2 2 1 1 1 1 0 0 0
+#mkdir -p results/current_run/l2x1x1_n8x4x4_i2_s1 && ./$folder/src/example 2.0 1.0 1.0 8 4 4 2 1 1 1 1 0 0 0
