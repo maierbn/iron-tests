e1ab37ebd908a5dccf6a1276ea0efec1f77cd35f
klotz-t <thomas.klotz@mechbau.uni-stuttgart.de>
2017-06-27 15:16:33 +0200

    updated local git

--- examples/example-0101/run_example.sh
+++ examples/example-0101/run_example.sh
@@ -12,4 +12,5 @@ cmake -DCMAKE_BUILD_TYPE=$folder -DOPENCMISS_BUILD_TYPE=$folder ..
 make
 cd ..
 echo "  running $folder"
-mkdir -p results/current_run && ./$folder/src/example
+#mkdir -p results/current_run && ./$folder/src/example
+mkdir -p results/current_run && ./$folder/src/example 100 100 100 2 2 2 1 0 10000 0.3 0.1
