a18ff84dc88094f2bd613bad6c915979a1780cc8
Andreas Hessenthaler <hessenthaler@mechbau.uni-stuttgart.de>
2017-06-30 17:11:18 +0200

    finished examples/example-0004

---
+++ examples/example-0004/src/cheart/run_cheart.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+# run 2D
+cheartsolver.out l4x2x0_n4x2x0_i1.P
+cheartsolver.out l4x2x0_n8x4x0_i1.P
+cheartsolver.out l4x2x0_n2x1x0_i2.P
+cheartsolver.out l4x2x0_n4x2x0_i2.P
+cheartsolver.out l4x2x0_n8x4x0_i2.P
+
+rm -f gmon.out
