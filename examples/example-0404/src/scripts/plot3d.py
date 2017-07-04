f8e288eb525c8a7975dcfa0e021c20368a929dee
emamy <Nehzat.Emamy@ipvs.uni-stuttgart.de>
2017-07-04 14:11:26 +0200

    remove some files

---
+++ examples/example-0404/src/scripts/plot3d.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#
+# script to plot Vm from all the exnode files as 2D surface for all timesteps in an animation
+
+from mpl_toolkits.mplot3d import Axes3D
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib import cm
+import exnode_reader
+from matplotlib import animation
+import sys
+
+def animate(t):
+  global foldername
+  ax1.clear()
+  
+  # get all exnode files sorted in that directory
+  files = exnode_reader.get_exnode_files(foldername)
+  
+  if t >= len(files):
+    return
+  iron_filename = files[t]
+  
+  iron_data = exnode_reader.parse_file(foldername+iron_filename, [["Vm", 1]])   # extract field Vm, component 1
+  
+  n = len(iron_data)
+  nx = int(np.sqrt(n))-1
+  ny = int(np.sqrt(n))-1
+  print "t=",t,"/",len(files),", n=",n,", nx=",nx, ", ny=", ny
+
+  X = range(nx+1)
+  Y = range(ny+1)
+  X, Y = np.meshgrid(X, Y)
+  Z_iron = X.copy()
+  for x in range(nx+1):
+    for y in range(ny+1):
+      Z_iron[x,y] = iron_data[x*(ny+1)+y]
+        
+  ax1.plot_surface(X, Y, Z_iron, cmap=cm.coolwarm, linewidth=1, antialiased=False, rstride=1, cstride=1)
+  ax1.set_zlim(-80,30)
+  plt.title("t = "+str(t*0.005))
+      
+# get folder name from command line argument
+foldername = "."
+if len(sys.argv) > 1:
+  foldername = sys.argv[1]
+
+# append slash if necessary
+if foldername[-1] != "/":
+  foldername = foldername + "/"
+
+# create figure
+fig = plt.figure(1)
+ax1 = fig.gca(projection='3d')
+plt.tight_layout()
+animate(0)
+      
+
+anim = animation.FuncAnimation(fig, animate,
+                               frames=1000, interval=20)
+# save to animation
+animation_name = "{}.mp4".format(foldername).replace("/", "_")
+anim.save(animation_name)
+print "saved to {}".format(animation_name)
+#plt.show()
+
+
