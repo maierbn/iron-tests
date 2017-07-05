bfc558107947b1160e60e8462e46b22d4523b0f1
Aaron <aaron.kraemer@ians.uni-stuttgart.de>
2017-07-05 10:15:31 +0200

    ode step number added

--- examples/example-0401/src/iron/FortranExample.f90
+++ examples/example-0401/src/iron/FortranExample.f90
@@ -116,7 +116,7 @@ PROGRAM MONODOMAINEXAMPLE
   REAL(CMISSRP), PARAMETER :: STIM_STOP = 0.10_CMISSRP
   REAL(CMISSRP) :: TIME_STOP = 1.50_CMISSRP
   ! ODE_TIME_STEP must exist. However, if ODE_NumberOfSteps is used, then ODE_TIME_STEP is neglected.
-  REAL(CMISSRP) :: ODE_TIME_STEP
+  REAL(CMISSRP) :: ODE_TIME_STEP = 0.0001
   REAL(CMISSRP) :: PDE_TIME_STEP
   REAL(CMISSRP), PARAMETER :: CONDUCTIVITY = 3.828_CMISSRP
 
@@ -221,7 +221,7 @@ PROGRAM MONODOMAINEXAMPLE
     CALL GET_COMMAND_ARGUMENT(10,COMMAND_ARGUMENT,ARGUMENT_LENGTH,STATUS)
     IF(STATUS>0) CALL HANDLE_ERROR("Error for command argument 10.")
     READ(COMMAND_ARGUMENT(1:ARGUMENT_LENGTH),*) ODE_NumberOfSteps
-    WRITE(*, '("ODE Number Of Steps: ", E14.7)') ODE_NumberOfSteps
+    WRITE(*, '("ODE Number Of Steps: ", I0)') ODE_NumberOfSteps
 
     inquire(file=CellmlFile, exist=fileExist)
     if (.not. fileExist) then
@@ -251,8 +251,7 @@ PROGRAM MONODOMAINEXAMPLE
     ! BASIS_LINEAR_SIMPLEX_INTERPOLATION=7 !<Linear Simplex interpolation specification \see BASIS_ROUTINES_InterpolationSpecifications,BASIS_ROUTINES
     ! BASIS_QUADRATIC_SIMPLEX_INTERPOLATION=8 !<Quadratic Simplex interpolation specification \see BASIS_ROUTINES_InterpolationSpecifications,BASIS_ROUTINES
     ! BASIS_CUBIC_SIMPLEX_INTERPOLATION=9 !<Cubic Simplex interpolation specification \see BASIS_ROUTINES_InterpolationSpecifications,BASIS_ROUTINES
-    PDE_TIME_STEP = 0.05_CMISSRP
-    ODE_TIME_STEP = 0.001_CMISSRP ! doesn't have to be in agreement with ODE_NumberOfSteps. ODE_NumberOfSteps is the important one, if it's used. 
+    PDE_TIME_STEP = 0.005_CMISSRP
     ODE_NumberOfSteps = 50        !
     TIME_STOP=3.00
     OUTPUT_FREQUENCY=1
@@ -563,9 +562,17 @@ PROGRAM MONODOMAINEXAMPLE
   !Get the first (DAE) solver
   CALL cmfe_Solver_Initialise(Solver,Err)
   CALL cmfe_Problem_SolverGet(Problem,CMFE_CONTROL_LOOP_NODE,1,Solver,Err)
-  !Set the DAE time step to by 10 us
+  
+  !-------------------------------------------------------------------------------------------
+  !Set the DAE time step to by 10 us (pardon?? what?!)
   CALL cmfe_Solver_DAETimeStepSet(Solver,ODE_TIME_STEP,Err)
+  !-------------------------------------------------------------------------------------------  
+  !Set the Number of ODE time steps. CARE: This makes cmfe_Solver_DAETimeStepSet() obsolete!
   CALL cmfe_Solver_DAEEulerForwardSetNSteps(Solver,ODE_NumberOfSteps,Err)
+  !-------------------------------------------------------------------------------------------
+  
+  !TODO make a test case for Heun and BDF.
+  
   !CALL cmfe_Solver_DAESolverTypeSet(Solver,CMFE_SOLVER_DAE_EXTERNAL,Err)
   CALL cmfe_Solver_OutputTypeSet(Solver,CMFE_SOLVER_NO_OUTPUT,Err)
   !CALL cmfe_Solver_OutputTypeSet(Solver,CMFE_SOLVER_PROGRESS_OUTPUT,Err)
