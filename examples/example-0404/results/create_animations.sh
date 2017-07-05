ed077204a5188e09df3dbcabc9bf5e0e5df26c42
emamy <Nehzat.Emamy@ipvs.uni-stuttgart.de>
2017-07-05 08:58:02 +0200

    add the template for convergence study of 1D problem

--- examples/example-0404/results/create_animations.sh
+++
@@ -1,7 +0,0 @@
-for directory in `ls current_run`
-do
-    echo $directory
-    python2.7 ../src/scripts/plot3d.py current_run/$directory
-done
-
-#python2.7 ../src/scripts/plot3d.py current_run/l1x1_n10x10_i1_s0_p1/
